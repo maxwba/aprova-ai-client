84c4d3649825b4e706ad4a97b3cd3c7f180f656c
Maxwell Martins Lima Monteiro <maxwba@HenriquosiPhone.localdomain>
2020-01-29 18:42:39 -0300

    minor changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Signup from "./auth/Signup";
 import Login from './auth/Login';
 // import axios from 'axios';
-// import { Link } from 'react-router-dom';
+// import { Link }sdsdsd from 'react-router-dom';
 
 const Home = () => {
   return (
