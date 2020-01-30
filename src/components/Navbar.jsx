d56a5d1a912071f044c2a5ccf225a72e5a8e5fb8
Nykolle Malone <nykolle.malone@hotmail.com>
2020-01-30 16:53:38 -0300

    layout done - need to fix footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,14 +48,14 @@ render () {
 
     this.fetchUser()
   return (
-<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+<nav className="navbar navbar-expand-lg navbar-light bg-transparent">
 <Link className="logo navbar-brand d-inline-block align-top ml-5 mt-1" exact to="/"> <img src="./images/logo.png"/> </Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse justify-content-end mr-5" id="navbarNav">
-    <ul class="navbar-nav">
-    <li class="nav-item active">
+  <div className="collapse navbar-collapse justify-content-end mr-5" id="navbarNav">
+    <ul className="navbar-nav">
+    <li className="nav-item active">
     <Link className="btn-login nav-link" to="/login" style={{ color: "gray", fontSize: 20 }}> Login </Link>
     </li>
     </ul>
