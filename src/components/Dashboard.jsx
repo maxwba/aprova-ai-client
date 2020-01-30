505e58d9adf1b0fa5f825fa0acc992aca9f3aa62
Vini <mvfv@outlook.com>
2020-01-30 19:57:07 -0300

    DrawMenu adicionado a rota Dashboard, versão inicial usando Material UI

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'; 
-import Menu from './Menu'
+// import Menu from './Menu'
+import Menu from './DrawMenu'
 import ClientDetails from './ClientDetails'
 
 const Dashboard = () => {
     return (
         <div>
+        {/* <Menu /> */}
         <Menu />
         <ClientDetails />
         </div>
