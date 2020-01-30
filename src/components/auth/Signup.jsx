ab0c0c16da196b684732bc540289c1852b691be7
Vini <mvfv@outlook.com>
2020-01-30 18:59:36 -0300

    Signup finalizada

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,136 +1,154 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import AuthService from './auth-service';
-import { Link } from 'react-router-dom';
-
-class Signup extends Component {
-    constructor(props) {
-      super(props);
-      this.state = { email: "", password: "", message: "" };
-      this.service = new AuthService();
-      this.handleFormSubmit = this.handleFormSubmit.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-    }
-    handleFormSubmit(event) {
-      event.preventDefault();
-      const email = this.state.email;
-      const password = this.state.password;
-      this.service
-        .signup(email, password)
-        .then(user => {
-          this.setState({
-            email: "",
-            password: ""
-          });
-          this.props.getUser(user);
-          // this.props.history.push("/projects"); <=== Nao sei o que isso faz
-        })
-        .catch(error => {
-          this.setState({
-            //message: error.response.data.message
-          });
-        });
-    }
-    handleChange(event) {
-      const { name, value } = event.target;
-      this.setState({ [name]: value });
-    }
-
-    render() {
-      console.log(this.props);
-      return (
-        <div>
-          
-          <form onSubmit={this.handleFormSubmit}>
-            <label>Email:</label>
-            <input
-              type="text"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-            <input type="submit" value="Signup" />
-          </form>
-
-          {this.state.message && <p>{this.state.message}</p>} {/* ESTILIZAR E COLOCAR NO LOGI */}
-          <p>
-            Already have account?
-            <Link to={"/"}> Login</Link>
-          </p>
-
-          <div class="field">
-  <label class="label">Name</label>
-  <div class="control">
-    <input class="input" type="text" placeholder="Text input"/>
-  </div>
-</div>
-
-<div class="field">
-  <label class="label">Username</label>
-  <div class="control has-icons-left has-icons-right">
-    <input class="input is-success" type="text" placeholder="Text input" value="bulma"/>
-    <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
-  </div>
-  <p class="help is-success">This username is available</p>
-</div>
-
-<div class="field">
-  <label class="label">Email</label>
-  <div class="control has-icons-left has-icons-right">
-    <input class="input is-danger" type="email" placeholder="Email input" value="hello@" />
-    <span class="icon is-small is-left">
-      <i class="fas fa-envelope"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-exclamation-triangle"></i>
-    </span>
-  </div>
-  <p class="help is-danger">This email is invalid</p>
-</div>
-
-<div class="field">
-  <label class="label">Subject</label>
-  <div class="control">
-    <div class="select">
-      <select>
-        <option>Select dropdown</option>
-        <option>With options</option>
-      </select>
-    </div>
-  </div>
-</div>
-
-<div class="field">
-  <label class="label">Message</label>
-  <div class="control">
-    <textarea class="textarea" placeholder="Textarea"></textarea>
-  </div>
-</div>
+import { _Link } from 'react-router-dom';
 
 
+//Material design UI
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container'; 
 
 
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        Aprova.AI
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 
-</div>
+const Signup = (props) =>{
+  const [email, handleEmail] = useState('')
+  const [password, handlePassword] = useState('')
+  const [message, handleMessage] = useState('')
+  const service = new AuthService()
 
-
-
-        
-      );
-    }
+  const handleFormSubmit = (event) => {
+    console.log(props)
+    event.preventDefault();
+    service
+      .signup(email, password)
+      .then(user => {
+        handleEmail('');
+        handlePassword('')
+        props.getUser(user);
+        props.history.push("/"); 
+      })
+      .catch(error => {
+        handleMessage(
+         error.response.data.message
+        )
+      });
   }
+
+  const classes = useStyles();
+  return(
+    <Container className='form' component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Signup
+        </Typography>
+        <form className={classes.form} onSubmit={handleFormSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => handleEmail(e.target.value)}
+
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Senha"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => handlePassword(e.target.value)}
+
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Lembrar"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+           Signup
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/login" variant="body2">
+                {"Já tem uma conta? Login"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
+
+
+
+
+}
   export default Signup;
   
   
