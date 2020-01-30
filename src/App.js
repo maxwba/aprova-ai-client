e088b783532f889d885352b9b5e868726825bc54
Nykolle Malone <nykolle.malone@hotmail.com>
2020-01-30 11:29:21 -0300

    Singn up, Login routes and navbar (need to fix)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
-// import { Switch, Route } from 'react-router-dom';;;
+import { Switch, Route } from 'react-router-dom';
+
 import Home from './components/Home'
+import Signup from './components/auth/Signup'
+import Login from './components/auth/Login'
+import AuthService from "./components/auth/auth-service";
+
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = { loggedInUser: null };
+    this.service = new AuthService();
+    this.getTheUser = this.getTheUser.bind(this);
   }
+
+  fetchUser() {
+    if (this.state.loggedInUser === null) {
+      this.service
+        .loggedin()
+        .then(response => {
+          this.setState({
+            loggedInUser: response
+          });
+        })
+        .catch(err => {
+          this.setState({
+            loggedInUser: false
+          });
+        });
+    }
+  }
+
   getTheUser(userObj) {
     this.setState({
       loggedInUser: userObj
     })
   }
+  
   render() {
+    this.fetchUser()
     return (
       <div className="App">
-        <Home/>
+        <Switch>
+        <Route exact path="/" component={Home}  user={this.props.user} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        </Switch>
       </div>
     );
   }
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -26,7 +26,7 @@ class Signup extends Component {
         })
         .catch(error => {
           this.setState({
-            message: error.response.data.message
+            //message: error.response.data.message
           });
         });
     }
@@ -39,6 +39,7 @@ class Signup extends Component {
       console.log(this.props);
       return (
         <div>
+          
           <form onSubmit={this.handleFormSubmit}>
             <label>Email:</label>
             <input
@@ -56,12 +57,77 @@ class Signup extends Component {
             />
             <input type="submit" value="Signup" />
           </form>
+
           {this.state.message && <p>{this.state.message}</p>} {/* ESTILIZAR E COLOCAR NO LOGI */}
           <p>
             Already have account?
             <Link to={"/"}> Login</Link>
           </p>
-        </div>
+
+          <div class="field">
+  <label class="label">Name</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Text input"/>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Username</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-success" type="text" placeholder="Text input" value="bulma"/>
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+  <p class="help is-success">This username is available</p>
+</div>
+
+<div class="field">
+  <label class="label">Email</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input is-danger" type="email" placeholder="Email input" value="hello@" />
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle"></i>
+    </span>
+  </div>
+  <p class="help is-danger">This email is invalid</p>
+</div>
+
+<div class="field">
+  <label class="label">Subject</label>
+  <div class="control">
+    <div class="select">
+      <select>
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Message</label>
+  <div class="control">
+    <textarea class="textarea" placeholder="Textarea"></textarea>
+  </div>
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+        
       );
     }
   }
