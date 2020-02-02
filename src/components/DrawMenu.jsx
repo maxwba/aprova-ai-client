efe512775730adac0bcf76b9100cdf15e8b35859
Vini <mvfv@outlook.com>
2020-02-02 02:11:45 -0300

    Botão criar client pront

--- src/components/DrawMenu.jsx
+++ src/components/DrawMenu.jsx
@@ -1,122 +1,118 @@
-import React, {useEffect} from 'react';
-import {Link} from 'react-router-dom'
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import BusinessIcon from '@material-ui/icons/Business';
-import ClientDetails from './ClientDetails'
-import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
-import HelpIcon from '@material-ui/icons/Help';
-import Avatar from '@material-ui/core/Avatar';
-import AuthService from './auth/auth-service';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import Axios from 'axios';
-import NewClient from './NewClient'
-import JobDetail from './JobDetail';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import BusinessIcon from "@material-ui/icons/Business";
+import ClientDetails from "./ClientDetails";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
+import HelpIcon from "@material-ui/icons/Help";
+import Avatar from "@material-ui/core/Avatar";
+import AuthService from "./auth/auth-service";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import Axios from "axios";
+import NewClient from "./NewClient";
+import JobDetail from "./JobDetail";
 
 const drawerWidth = 240;
 
-
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: 36
   },
   hide: {
-    display: 'none',
+    display: "none"
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: 'nowrap',
+    whiteSpace: "nowrap"
   },
   drawerOpen: {
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   drawerClose: {
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
-    overflowX: 'hidden',
+    overflowX: "hidden",
     width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1
+    }
   },
   toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
     padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
+    ...theme.mixins.toolbar
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+    padding: theme.spacing(3)
   },
 
   large: {
     width: theme.spacing(7),
-    height: theme.spacing(7),
-  },
+    height: theme.spacing(7)
+  }
 }));
 
 export default function MiniDrawer(props) {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
-  const [logout, handleLogout] = React.useState(props.loggedInCompany)
-  const [company, handleCompany] = React.useState([])
-  const [client, handleClient] = React.useState(false)
+  const [logout, handleLogout] = React.useState(props.loggedInCompany);
+  const [company, handleCompany] = React.useState([]);
+  const [client, handleClient] = React.useState(false);
 
-  
-
-   useEffect(() => {
-    async function getAllClient(){
-      Axios.get('http://localhost:5000/api/client')
-      .then(responseFromApi => {
-        console.log(responseFromApi)
-        handleCompany(responseFromApi.data)
-      })
+  useEffect(() => {
+    async function getAllClient() {
+      Axios.get("http://localhost:5000/api/client").then(responseFromApi => {
+        console.log(responseFromApi);
+        handleCompany(responseFromApi.data);
+      });
     }
-    getAllClient()
-  }, [open])
+    getAllClient();
+  }, [open]);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -127,21 +123,17 @@ export default function MiniDrawer(props) {
   };
 
   const handleChangeClient = () => {
-    handleClient(true)
-  }
+    handleClient(true);
+  };
 
-  const service = new AuthService()
-const logoutCompany = (props) => {
-  if(logout){
-    service
-    .logout()
-    .then(response => {
-      handleLogout(null)
-    })
-  }
-}
-
-
+  const service = new AuthService();
+  const logoutCompany = props => {
+    if (logout) {
+      service.logout().then(response => {
+        handleLogout(null);
+      });
+    }
+  };
 
   return (
     <div className={classes.root}>
@@ -149,7 +141,7 @@ const logoutCompany = (props) => {
       <AppBar
         position="fixed"
         className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
+          [classes.appBarShift]: open
         })}
       >
         <Toolbar>
@@ -159,13 +151,13 @@ const logoutCompany = (props) => {
             onClick={handleDrawerOpen}
             edge="start"
             className={clsx(classes.menuButton, {
-              [classes.hide]: open,
+              [classes.hide]: open
             })}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            <img src="./images/logo.png"/> 
+            <img src="./images/logo.png" />
           </Typography>
         </Toolbar>
       </AppBar>
@@ -173,37 +165,45 @@ const logoutCompany = (props) => {
         variant="permanent"
         className={clsx(classes.drawer, {
           [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
+          [classes.drawerClose]: !open
         })}
         classes={{
           paper: clsx({
             [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
+            [classes.drawerClose]: !open
+          })
         }}
       >
         <div className={classes.toolbar}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
         <List>
-        <Avatar alt="Vinivibe" src="./images/batman.png" className={classes.large} />
+          <Avatar
+            alt="Vinivibe"
+            src="./images/batman.png"
+            className={classes.large}
+          />
         </List>
         <Divider />
         <Divider />
         <List>
-          {company.map((text, index) =>{
-            const {name} = text;
-            const key = text+'-'+index
-              return(
-                <ListItem button key={key}>
-                  <ListItemIcon>{ <BusinessIcon/>}</ListItemIcon>
-                  <ListItemText primary={name} />
-                </ListItem>
-              )
-          } )}
+          {company.map((text, index) => {
+            const { name } = text;
+            const key = text + "-" + index;
+            return (
+              <ListItem button key={key}>
+                <ListItemIcon>{<BusinessIcon />}</ListItemIcon>
+                <ListItemText primary={name} />
+              </ListItem>
+            );
+          })}
         </List>
         <Divider />
         <List>
@@ -214,34 +214,41 @@ const logoutCompany = (props) => {
             </ListItem>
           ))} */}
 
-            
-            <ListItem button onClick={handleChangeClient}>
-              <ListItemIcon> <AddToPhotosIcon /> </ListItemIcon>
-              <ListItemText>Add Company</ListItemText>
-            </ListItem>
-            
+          <ListItem button onClick={handleChangeClient}>
+            <ListItemIcon>
+              {" "}
+              <AddToPhotosIcon />{" "}
+            </ListItemIcon>
+            <ListItemText>Add Company</ListItemText>
+          </ListItem>
 
-            <Link to='/help'>
-            <ListItem >
-              <ListItemIcon> <HelpIcon /></ListItemIcon>
-              <ListItemText >Help</ListItemText>
+          <Link to="/help">
+            <ListItem>
+              <ListItemIcon>
+                {" "}
+                <HelpIcon />
+              </ListItemIcon>
+              <ListItemText>Help</ListItemText>
             </ListItem>
-            </Link>
-            <Link to='/'>
-            <ListItem >
-              <ListItemIcon><ExitToAppIcon /></ListItemIcon>
-              <ListItemText onClick={() => logoutCompany(props)} >Logout</ListItemText>
+          </Link>
+          <Link to="/">
+            <ListItem>
+              <ListItemIcon>
+                <ExitToAppIcon />
+              </ListItemIcon>
+              <ListItemText onClick={() => logoutCompany(props)}>
+                Logout
+              </ListItemText>
             </ListItem>
-            </Link>
-
+          </Link>
         </List>
       </Drawer>
 
-        <main className={classes.content}>
+      <main className={classes.content}>
         <div className={classes.toolbar} />
 
         {/* Condiction to change the view */}
-        { client ? <NewClient /> : <JobDetail />  }
+        {client ? <NewClient /> : <JobDetail />}
       </main>
     </div>
   );
