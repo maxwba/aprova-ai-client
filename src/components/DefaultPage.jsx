b6c43480010e87d5e09cbe110fbb84dd142c483d
Maxwell Martins Lima Monteiro <maxwba@HenriquosiPhone.localdomain>
2020-02-03 12:57:29 -0300

    Filtered clients complete

---
+++ src/components/DefaultPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DefaultPage = props => {
+    return (
+      <div>
+        <h1>Pagina princial</h1>
+      </div>
+    );
+  };
+  
+  export default DefaultPage;
+  
