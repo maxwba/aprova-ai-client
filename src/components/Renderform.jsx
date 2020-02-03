47a65ae947b7a5518c21cb5d68de10fb661414bf
Vini <mvfv@outlook.com>
2020-02-03 12:38:20 -0300

    novas

--- src/components/Renderform.jsx
+++ src/components/Renderform.jsx
@@ -5,7 +5,7 @@ import { Theme as MuiTheme } from "rjsf-material-ui";
 import { Container, Typography, Box, Button } from "@material-ui/core";
 
 const RenderForm = () => {
-  const [forms, handleForm] = React.useState([]);
+  const [forms, handleForm] = React.useState();
   const Form = withTheme(MuiTheme);
 
   const convertArrayToObject = array => {
@@ -20,24 +20,45 @@ const RenderForm = () => {
 
   const getAllForm = () => {
     Axios.get("http://localhost:5000/api/form").then(responseFromApi => {
-      console.log(responseFromApi);
+      console.log(`=============>`, responseFromApi);
       handleForm(responseFromApi.data);
     });
   };
+
+  const novoform = forms.map(({ properties }, index) => {
+    return properties;
+  });
 
   const formSchema = {
     title: "Formulario criado pelo user",
     description: "User cria para empresa responder",
     type: "string",
-    properties: convertArrayToObject(forms)
+    properties: novoform
   };
 
-  console.log(`=========>`, forms);
-
+  getAllForm();
   // Preciso fazer um map no forms pois está vindo tudo junto e misturado então precisamos renderizar um por um
   return (
     <Container>
-      <Form schema={formSchema} onSubmit={console.log("foi")} />
+      <Form schema={formSchema} onSubmit={``} />
+      <div className="vini">
+        <form onSubmit={``}>
+          <label>Formulario de exemplo</label>
+          {forms &&
+            forms.map(({ title, description, type }, index) => {
+              return (
+                <Box key={index}>
+                  <label>{title}</label>
+                  <br />
+                  <input type={type} name={title} value={description} />
+                  <br />
+                  <br />
+                </Box>
+              );
+            })}
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </Container>
   );
 };
